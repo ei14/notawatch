c05622ebdb7a96a87da9dd7c1c3092ee1e57b418
ei14 <tbk283@gmail.com>
2020-06-24 15:09:53 -0500

    Added some code documentation

--- firmware/main.c
+++ firmware/main.c
@@ -1,43 +1,77 @@
+/*
+NotAWatch Firmware
+2020 edition
+Created by Thomas Kaldahl
+*/
+
 #include <avr/io.h>
 #include <util/delay.h>
 
 #define F_CPU 4000000
 
-long long px = 0;
-long now;
+long long px = 0; // bitwise storage of screen state
+long now; // time in centiseconds
+long then; // general purpose timestamp storage
+char inc; // number of centiseconds to add at the end of the frame
+char i; // general purpose loop iterator
+char mode; /* watch mode.
+	0 - sleep
+	1 - time
+	2 - date
+	3 - time, loading set time
+	4 - set time: centisecond
+	5 - set time: second
+	6 - set time: minute
+	7 - set time: hour
+	8 - set time: day
+	9 - set time: month
+	10 - time, loading stopwatch
+	11 - stopwatch: paused
+	12 - stopwatch: running
+	13 - stopwatch: showing lap, running
+	14 - stopwatch: showing lap, paused
+	15 - time, loading game menu
+	16 - game menu
+	17 - snake
+	18 - snake game over
+	19 - connect 4
+	20 - connect 4 won
+	21 - connect 4, loading sleep
+*/
+
+void redraw(void) {
+	for(i = 0; i < 64; i++) {
+		// set row bit
+		if(px & (1 << i)) PORTA |= 0b0100;
+		else PORTA &= 0b1011;
+
+		// set column bit
+		if(i%9 == 0) PORTA |= 0b1000;
+		else PORTA &= 0b0111;
+
+		// tick shift register clocks
+		PORTA |= 0b0001;
+		PORTA &= 0b1110;
+
+		if(i%8 == 0) {
+			// submit row
+			PORTA |= 0b0010;
+			PORTA &= 0b1101;
+		}
+	}
+}
 
 int main(void) {
-	int r = 0;
-	int c = 0;
-	int s = 0;
-	int t = 0;
-
-	DDRA |= (1 << PA0);
-	DDRA |= (1 << PA1);
-	DDRA |= (1 << PA2);
-	DDRA |= (1 << PA3);
+	DDRA = 0b1111;
 	while(1) {
-		if(s ^ (c & 1) ^ (r & 1)) PORTA |= 1 << PA2;
-		else PORTA &= ~(1 << PA2);
-		if(c == r) PORTA |= 1 << PA3;
-		else PORTA &= ~(1 << PA3);
-		PORTA |= 1 << PA0;
-		PORTA &= ~(1 << PA0);
-		c++;
-		if(c == 8) {
-			c = 0;
-			PORTA |= 1 << PA1;
-			PORTA &= ~(1 << PA1);
-			r++;
-			if(r == 8) {
-				r = 0;
-			}
+		inc = 1;
+
+		switch(mode) {
+
 		}
-		t++;
-		if(t == 20000) {
-			t = 0;
-			s ^= 1;
-		}
+
+		redraw();
+		now += inc;
 	}
 
 	return 1;
