08dddd452a0ca82dcd645d5a0bef8711575919fc
ei14 <tbk283@gmail.com>
2020-11-03 22:01:28 -0600

    Decided to start simple because I don't have much time in college

--- firmware/main.asm
+++ firmware/main.asm
@@ -9,16 +9,16 @@
 .equ ser_a = pa0
 .equ ser_b = pa1
 .equ rfsh = pa2
-.equ shft = pa2
+.equ shft = pa3
 .equ btn_a = pb2
 .equ btn_b = pa7
 
-.def ns = r26
-.def us = r27
-.def ms = r28
-.def sc = r29
-.def mn = r30
-.def dy = r31
+.def ns = r20
+.def us = r21
+.def ms = r22
+.def sc = r23
+.def mn = r24
+.def dy = r25
 
 .org 0
 	sbi ddra, ser_a
@@ -29,33 +29,34 @@
 sleep:
 
 time:
-	ldi r25, 6
+	
+	ldi r19, 6
 	rcall inc_time
 	rjmp time
 
-inc_time: ; r25: amount to be added. This function needs 3 extra to the amount.
-	add ns, r25
+inc_time: ; r19: amount to be added. This function needs 3 extra to the amount.
+	add ns, r19
 	brcc return
-	ldi r25, 4
-	add ns, r25
+	ldi r19, 4
+	add ns, r19
 	inc us
 	brne return
-	ldi r25, 4
-	add ns, r25
+	ldi r19, 4
+	add ns, r19
 	inc ms
 	brne return
-	ldi r25, 4
-	add ns, r25
+	ldi r19, 4
+	add ns, r19
 	inc sc
 	brne return
-	ldi r25, 4
-	add ns, r25
+	ldi r19, 4
+	add ns, r19
 	inc mn
 	brne return
-	ldi r25, 4
-	add ns, r25
-	inc dy
-	brne return
+	ldi r19, 3
+	add ns, r19
+	
+	ret
 
 return:
 	ret
